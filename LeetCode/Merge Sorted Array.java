ea1cebf964ecbd2912b95b81e36c7d60f32b1db8
Cubist4707 <cubist4707@gmail.com>
2026-07-28 13:49:59 +0900

    Solve LeetCode Merge Sorted Array

---
+++ LeetCode/Merge Sorted Array.java
@@ -0,0 +1,13 @@
+class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int idx1 = m - 1, idx2 = n - 1;
+        for (int i = nums1.length - 1; i >= 0; i--) {
+            if (!(idx1 >= 0 && idx2 >= 0)) break;
+
+            if (nums1[idx1] >= nums2[idx2]) nums1[i] = nums1[idx1--];
+            else nums1[i] = nums2[idx2--];
+        }
+
+        while (idx2 >= 0) nums1[idx2] = nums2[idx2--];
+    }
+}
