c7f7f3b7bb32c0f0b1b913cfdcfbe14cd7083e1c
Cubist4707 <cubist4707@gmail.com>
2026-07-29 13:39:44 +0900

    Refactor HashSet 제거 및 투포인터 중복 스킵 로직 추가

--- LeetCode/3Sum.java
+++ LeetCode/3Sum.java
@@ -2,24 +2,29 @@ class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
         Arrays.sort(nums);
 
-        HashSet<List<Integer>> set = new HashSet<>();
+        List<List<Integer>> ans = new ArrayList<>();
         int N = nums.length;
         for (int i = 0; i < N - 2; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
+
             int left = i + 1;
             int right = N - 1;
 
             while (left < right) {
                 int tmp = nums[i] + nums[left] + nums[right];
                 if (tmp == 0) {
-                    set.add(Arrays.asList(nums[i], nums[left], nums[right]));
+                    ans.add(Arrays.asList(nums[i], nums[left], nums[right]));
+                    while (left < right && nums[left] == nums[left + 1]) left++;
                     left++;
+                    while (left < right && nums[right] == nums[right - 1]) right--;
+                    right--;
                 }
                 else if (tmp < 0) left++;
                 else if (tmp > 0) right--;
             }
         }
 
-        List<List<Integer>> ans = new ArrayList<>(set);
+
         return ans;
     }
 }
